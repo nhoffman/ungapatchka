642ce8395269690426ac5524f90b2e452f40ef6f
Noah Hoffman <noah.hoffman@gmail.com>
2014-09-30 21:30:48 -0700

    add setup.py check_version

--- setup.py
+++ setup.py
@@ -2,7 +2,29 @@ import glob
 import os
 import subprocess
 
+from distutils.core import Command
 from setuptools import setup, find_packages
+
+
+class CheckVersion(Command):
+    description = 'Confirm that the stored package version is correct'
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        with open('ungapatchka/data/ver') as f:
+            stored_version = f.read().strip()
+
+        git_version = subprocess.check_output(
+            ['git', 'describe', '--tags', '--dirty']).strip()
+
+        assert stored_version == git_version
+        print 'the current version is', stored_version
 
 subprocess.call(
     ('git describe --tags --dirty > ungapatchka/data/ver.tmp'
@@ -24,7 +46,8 @@ params = {'author': 'Your name',
           },
           'version': __version__,
           'package_data': {'ungapatchka': package_data},
-          'test_suite': 'tests'
+          'test_suite': 'tests',
+          'cmdclass': {'check_version': CheckVersion}
           }
 
 setup(**params)
